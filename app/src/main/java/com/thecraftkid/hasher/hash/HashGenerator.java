b72867c7b2c45e6cbc1cca9a9c83adeddeb6f525
Willie Chalmers III <willieechalmers@gmail.com>
2018-01-03 02:33:14 -0600

    Create HashGenerator

---
+++ app/src/main/java/com/thecraftkid/hasher/hash/HashGenerator.java
@@ -0,0 +1,41 @@
+package com.thecraftkid.hasher.hash;
+
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ * A class that generates MD5, SHA1, and SHA256 hashes.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public final class HashGenerator {
+
+    private HashGenerator() {
+    }
+
+    /**
+     * Returns a MD5 hash of the given input.
+     */
+    public static String generateMD5(String input) {
+        return decodeBytes(DigestUtils.md5(input));
+    }
+
+    /**
+     * Returns a SHA1 hash of the given input.
+     */
+    public static String generateSHA1(String input) {
+        return decodeBytes(DigestUtils.sha1(input));
+    }
+
+    /**
+     * Returns a SHA256 hash of the given input.
+     */
+    public static String generateSHA256(String input) {
+        return decodeBytes(DigestUtils.sha256(input));
+    }
+
+    private static String decodeBytes(byte[] bytes) {
+        return new String(Hex.encodeHex(bytes));
+    }
+}
