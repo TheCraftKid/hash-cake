81704a12eeba8e9f511b8ca186ee680832bc210f
Willie Chalmers III <willieechalmers@gmail.com>
2018-01-03 03:03:33 -0600

    Add NonNull annotations to HashGenerator

--- app/src/main/java/com/thecraftkid/hasher/hash/HashGenerator.java
+++ app/src/main/java/com/thecraftkid/hasher/hash/HashGenerator.java
@@ -1,5 +1,7 @@
 package com.thecraftkid.hasher.hash;
 
+import android.support.annotation.NonNull;
+
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
 
@@ -17,6 +19,7 @@ public final class HashGenerator {
     /**
      * Returns a MD5 hash of the given input.
      */
+    @NonNull
     public static String generateMD5(String input) {
         return decodeBytes(DigestUtils.md5(input));
     }
@@ -24,6 +27,7 @@ public final class HashGenerator {
     /**
      * Returns a SHA1 hash of the given input.
      */
+    @NonNull
     public static String generateSHA1(String input) {
         return decodeBytes(DigestUtils.sha1(input));
     }
@@ -31,10 +35,12 @@ public final class HashGenerator {
     /**
      * Returns a SHA256 hash of the given input.
      */
+    @NonNull
     public static String generateSHA256(String input) {
         return decodeBytes(DigestUtils.sha256(input));
     }
 
+    @NonNull
     private static String decodeBytes(byte[] bytes) {
         return new String(Hex.encodeHex(bytes));
     }
