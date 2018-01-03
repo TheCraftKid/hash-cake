be3d91e94a64f9e55882ef9537db42ee0f1b1b6c
Willie Chalmers III <willieechalmers@gmail.com>
2018-01-03 01:05:05 -0600

    Create HashOption enum class

---
+++ app/src/main/java/com/thecraftkid/hasher/hash/HashOption.kt
@@ -0,0 +1,13 @@
+package com.thecraftkid.hasher.hash
+
+/**
+ * The supported set of hashing functions this app supports.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+enum class HashOption {
+    MD5,
+    SHA1,
+    SHA256;
+}
